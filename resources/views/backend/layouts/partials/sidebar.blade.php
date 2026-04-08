764886c214b17dde65d02b57c70475d1a229ff27
AdnanPanakkattil <muhammedadnan1247@gmail.com>
2026-04-08 19:01:19 +0530

    back end layout

--- resources/views/backend/layouts/partials/sidebar.blade.php
+++ resources/views/backend/layouts/partials/sidebar.blade.php
@@ -1,100 +1,90 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-  <div class="app-brand demo">
+  <!-- Logo Section -->
+  <div class="app-brand demo py-3">
     <a href="index.html" class="app-brand-link">
       <span class="app-brand-logo demo">
-        <svg width="32" height="22" viewBox="0 0 32 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-            fill="#7367F0" />
-          <path
-            opacity="0.06"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
-            fill="#161616" />
-          <path
-            opacity="0.06"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
-            fill="#161616" />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-            fill="#7367F0" />
-        </svg>
+        <!-- <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
+          <path d="M16 2L28.1244 9V23L16 30L3.87564 23V9L16 2Z" stroke="#2e7d52" stroke-width="2.5" />
+          <path d="M16 8L22.9282 12V20L16 24L9.0718 20V12L16 8Z" fill="#2e7d52" opacity="0.3" />
+        </svg> -->
+        <img src="{{ asset('assets/imglogo/logo2.png') }}" alt="" height="40px">
       </span>
-      <span class="app-brand-text demo menu-text fw-bold">Admin</span>
+      <span class="app-brand-text demo menu-text fw-bold ms-2 text-dark" style="font-size: 24px;">Furnixar</span>
     </a>
 
-    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
-      <i class="ti menu-toggle-icon d-none d-xl-block align-middle"></i>
-      <i class="ti ti-x d-block d-xl-none ti-md align-middle"></i>
-    </a>
   </div>
 
   <div class="menu-inner-shadow"></div>
 
   <ul class="menu-inner py-1">
-    <!-- Apps & Pages -->
-    <li class="menu-header small">
-      <span class="menu-header-text" data-i18n="Apps & Pages">Menus</span>
-    </li>
-    <li class="menu-item active open">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-solid fa-table-columns"></i>
+    <!-- Section: Main -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">dashboards</span></li>
+    <li class="menu-item active">
+      <a href="javascript:void(0);" class="menu-link">
+        <i class="menu-icon ti ti-smart-home"></i>
         <div data-i18n="Dashboards">Dashboards</div>
-      </a>
-    </li>
-    <li class="menu-item">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-solid fa-newspaper"></i>
-        <div data-i18n="Articles">Articles</div>
+
       </a>
     </li>
 
+    <!-- Section: Web Apps -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Web Apps</span></li>
     <li class="menu-item">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-solid fa-stethoscope"></i>
-        <div data-i18n="Doctors">Doctors</div>
+      <a href="javascript:void(0);" class="menu-link">
+        <i class="menu-icon ti ti-layout-grid"></i>
+        <div data-i18n="Apps">Apps</div>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link">
+        <i class="menu-icon ti ti-files"></i>
+        <div data-i18n="Nested Menu">Nested Menu</div>
       </a>
     </li>
 
-    <li class="menu-item">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-regular fa-building"></i>
-        <div data-i18n="Department">Department</div>
+    <li class="menu-item" id="service_main_menu">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons ti ti-server-cog"></i>
+        <div data-i18n="Services"> Services </div>
       </a>
-    </li>
-    <li class="menu-item">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-solid fa-network-wired"></i>
-        <div data-i18n="Branch">Branch</div>
-      </a>
+      <ul class="menu-sub">
+        <li class="menu-item" id="insurance_sub_menu">
+          <a href="" class="menu-link">
+            <div data-i18n="Insurance Services"> Insurance Service</div>
+          </a>
+        </li>
+        <li class="menu-item" id="xray_sub_menu">
+          <a href="" class="menu-link">
+            <div data-i18n="X-Ray Service"> X-Ray Service</div>
+          </a>
+        </li>
+
+
+      </ul>
     </li>
 
-    <li class="menu-item">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-solid fa-robot"></i>
-        <div data-i18n="Robot">Robot</div>
+    <li class="menu-item" id="laboratory_main_sub_menu">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <div data-i18n="Laboratory"> Laboratory </div>
       </a>
+      <ul class="menu-sub">
+
+        <li class="menu-item" id="laboratory_sub_menu">
+          <a href="" class="menu-link">
+            <div data-i18n="Laboratory">Laboratory</div>
+          </a>
+        </li>
+
+        <li class="menu-item" id="laboratory-package_sub_menu">
+          <a href="" class="menu-link">
+            <div data-i18n="Package">Package</div>
+          </a>
+        </li>
+
+      </ul>
     </li>
-    <li class="menu-item">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-solid fa-wrench"></i>
-        <div data-i18n="Service">Service</div>
-      </a>
-    </li>
-    <li class="menu-item">
-      <a href="" class="menu-link">
-        <i class="menu-icon fa-solid fa-chart-line"></i>
-        <div data-i18n="Activity">Activity</div>
-      </a>
-    </li>
-    <!-- e-commerce-app menu start -->
 
   </ul>
+
+
 </aside>
