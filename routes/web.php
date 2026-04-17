60e446c95619273cdf15e114a3734b083141304e
AdnanPanakkattil <muhammedadnan1247@gmail.com>
2026-04-17 16:28:02 +0530

    v1

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect('/admin/dashboard');
 });
 
 // Route::get('cart', function () {
